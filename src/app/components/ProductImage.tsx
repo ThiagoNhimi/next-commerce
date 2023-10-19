41fa7d4210c20fe8a872a2458ec6f70333b50649
Thiago Nhimi <thinhimi816@gmail.com>
2023-10-19 20:29:53 -0300

    Estilizando produto

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { ProductType } from "@/types/ProductType"
+import {useState} from 'react'
+import Image from "next/image";
+
+type ProductImageProps = {
+    product: ProductType;
+    fill?: boolean;
+};
+
+export default function ProductImage({product, fill}: ProductImageProps) {
+    const [loading, setLoading] = useState(true);
+    return fill ?( 
+        <Image
+            src={product.image}
+            fill
+            alt={product.title}
+            className={`object-cover border border-teal-500 border-2 ${
+                loading ? 'scale-110 blur-3xl grayscale'
+                : 'scale-100 blur-0 grayscale-0'
+            } `}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    ):
+    ( 
+        <Image
+            src={product.image}
+            width={400}
+            height={700}
+            alt={product.title}
+            className={`object-cover ${
+                loading ? 'scale-110 blur-3xl grayscale'
+                : 'scale-100 blur-0 grayscale-0'
+            } `}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from './components/navbar'
+import Navbar from './components/NavBar'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={clsx(inter.className, 'bg-slate-600')}>
       <Navbar />
-      <main className='bg-slate-400 h-screen py-16'>{children}</main>
+      <main className=' h-screen py-16'>{children}</main>
       </body>
      
     </html>
